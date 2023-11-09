79f2d41a54c8e3340e61a3bfa3705c6934f7dac7
jyothish.reddy <jyothish.reddy@covalensedigital.com>
2023-11-09 13:32:06 +0530

    initial CICD demo7

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
-import syncfusion_flutter_pdfviewer_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
 }
