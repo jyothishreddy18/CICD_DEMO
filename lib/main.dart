387d91d92f04b38ee240847ba0d3a8b782e50767
jyothish.reddy <jyothish.reddy@covalensedigital.com>
2023-11-09 15:39:45 +0530

    initial CICD demo15

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'Views/Login.dart';
-
 void main() {
-  // ColorConstants.initialize();
-  runApp(
-    MaterialApp(home: MyApp(),)
-    // MultiProvider(
-    //   providers: [
-    //     ChangeNotifierProvider(create: (_) => AuthViewModel()),
-    //   ],
-    //   child:  MyApp(),
-    // ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        home: LoginScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: LoginScreen(),
+    );
   }
 }
